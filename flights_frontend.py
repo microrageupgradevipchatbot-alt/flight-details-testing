f1b1477ed9df2a2fdcdb42304d8888b0bc6e9a35
microrageupgradevipchatbot-alt <microrageupgradevipchatbot@gmail.com>
2026-03-25 18:10:41 +0500

    Update flights_frontend.py

--- flights_frontend.py
+++ flights_frontend.py
@@ -222,11 +222,13 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-
 components.html("""
-<!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+var Tawk_API=Tawk_API||{};
+Tawk_API.onLoad = function(){
+    window.parent.Tawk_API = Tawk_API;
+};
+
 (function(){
 var s1=document.createElement("script"),
 s0=document.getElementsByTagName("script")[0];
@@ -237,5 +239,4 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-<!--End of Tawk.to Script-->
 """, height=0)
